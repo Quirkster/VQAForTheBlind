c1b1424b56871c6ec407653614d41d897cd24e82
John Ross <jross112@terpmail.umd.edu>
2026-05-08 13:21:44 -0400

    Refactor convert_to_h5.py to use argparse
    Added command-line arguments for input directory and output file name.

--- experiments/convert_to_h5.py
+++ experiments/convert_to_h5.py
@@ -1,13 +1,20 @@
 import h5py
 import cv2
 import glob
+import argparse
+
+# use --input_dir to specify the folder with all of the images
+# use --output_file to name the output h5 file, default is dataset.h5
+parser = argparse.ArgumentParser(description='Convert images to HDF5 format')
+parser.add_argument('--input_dir', type=str, required=True, help='Directory containing input images')
+parser.add_argument('--output_file', type=str, default='dataset.h5', help='Output HDF5 file name')
+args = parser.parse_args()
 
 # 1. Prepare image paths
-image_paths = glob.glob('C:\\Users\\jfr11\\Downloads\\val\\val\\*.jpg') # replace with your image paths
+image_paths = glob.glob(args.input_dir + '/*.jpg') # replace with your image paths
 img_height, img_width = 128, 128 # Define standard size
-i = 0
 # 2. Create HDF5 file
-with h5py.File('dataset.h5', 'w') as hdf:
+with h5py.File(args.output_file, 'w') as hdf:
     # Create dataset for images and labels
     dset = hdf.create_dataset('images', (len(image_paths), img_height, img_width, 3), dtype='uint8')
 
@@ -18,9 +25,10 @@ with h5py.File('dataset.h5', 'w') as hdf:
         # 4. Save to HDF5
         dset[i] = img
 
-f = h5py.File('dataset.h5', 'r')
+# example to read an image from the h5 file, you can run this part separately after the conversion is done
+f = h5py.File(args.output_file, 'r')
 dset = f['images']
 from matplotlib import pyplot as plt
-plt.imshow(dset[6], interpolation='nearest')
+plt.imshow(dset[0], interpolation='nearest') # change dset[0] to any index to view a different image
 plt.show()
 f.close()
